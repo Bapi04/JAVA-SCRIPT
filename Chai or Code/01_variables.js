c5aa18a1d6c322ee95170663857ff50808abca71
bapiranjan <bapiranjan8@gmail.com>
2023-05-27 23:13:20 +0530

    data types and variables

---
+++ Chai or Code/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let acountEmail = "Bapi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Sambalpur"
+let accountState;
+
+// accountId = 2  //Not allowed
+
+acountEmail="HC@gmail.com"
+accountPassword = "212121"
+accountCity = "Bargarh"
+
+console.log(accountId);
+
+console.table([accountId,acountEmail,accountPassword,accountCity,accountState])
+
+//var not going to use ok
---
+++ Chai or Code/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newr version
+
+// alert(3+3) //we are using Node.js not browser
+
+console.log(3+3)
+
+let name = "bapi"
+let age =  18
+let isLoggedIn = false
+
+
+//Number => 2^53
+//bigint
+//string ==> " "
+//boolean ==> true/false
+//null ==> standalone value
+//undefined => not value assigned
+//symbol => unique 
+//object
+
+console.log(typeof("bapi"))
+console.log(typeof(null)) // in java script null is type of object
+console.log(typeof(undefined)) // it self a type 
