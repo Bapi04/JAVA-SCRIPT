3a1689cdb245961a8eb61876094038bd5431444f
bapiranjan <bapiranjan8@gmail.com>
2023-06-01 11:01:01 +0530

    op

---
+++ 04_operations.js
@@ -0,0 +1,26 @@
+let value=3
+let negvalue = -value
+// console.log(negvalue);
+
+//Basic math operations
+//.....................
+
+let str1="hello"
+let str2=" bapi"
+let str3=str1+str2
+console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter=100;
+gameCounter++;
+//++gameCounter;
+console.log(gameCounter);
+
