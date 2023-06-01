b0df4b3b8b49b25f20ec4b312ed5c2d34fd2114d
bapiranjan <bapiranjan8@gmail.com>
2023-06-01 10:36:12 +0530

    03_conversionOpr

---
+++ Chai or Code/03_conversionOpr.js
@@ -0,0 +1,24 @@
+let score = "mxm"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+//"33"==>33
+//"33acc"==>NaN
+//true==>1, false==>0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1==>true; 0==>false
+//""==>false
+//"bapi"==>true
+
+let someNumber = 33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
